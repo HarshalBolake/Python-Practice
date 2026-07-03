47ce6e6a3caa8ad19d8bff5c6fc7947e0e6fa723
Harshal Bolake <n1433115@my.ntu.ac.uk>
2026-07-03 21:41:59 +0100

    added module folder and venv code

---
+++ module/app.py
@@ -0,0 +1,13 @@
+import mymodule as mm
+from mymodule import person1
+import platform
+
+mm.greeting("Harshal")
+
+# a = mm.person1["age"]
+# print(a)
+print(person1["age"])
+
+
+x = platform.system()
+print(x)
