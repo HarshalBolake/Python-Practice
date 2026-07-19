cfbe316aff1f53c3fbf45e934bfedbf708fc6a7d
Harshal Bolake <n1433115@my.ntu.ac.uk>
2026-07-19 23:39:55 +0100

    added class&object with __init__() method

---
+++ OOP/__init__()method.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self,name):
+        self.name = name
+
+
+    def greet(self):
+        return "Hello "+self.name
+
+    def welcome(self):
+        message = self.greet()
+        print(message + " welcome to python")
+
+P1 = Person("Harshal")
+P1.welcome()
+
---
+++ OOP/class&object.py
@@ -0,0 +1,11 @@
+class MyClass:
+    x = 10
+
+mc1 = MyClass()
+mc2 = MyClass()
+mc3 = MyClass()
+print(mc1.x)
+print(mc2.x)
+print(mc3.x)
+
+
